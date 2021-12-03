51d58b92a2e915756ebd4c874d106ea50545f36f
Ganondorf <86022295+GanOnsauce@users.noreply.github.com>
2021-12-03 16:21:35 -0600

    ??

---
+++ Chapters/Graphing.playgroundchapter/Pages/Graph.playgroundpage/main.swift
@@ -0,0 +1,17 @@
+//#-hidden-code
+//
+//  main.swift
+//  
+//  Copyright © 2016-2020 Apple Inc. All rights reserved.
+//
+//#-end-hidden-code
+//#-code-completion(module, hide, Swift)
+//#-code-completion(identifier, hide, _setup(), AbstractPointChartable)
+//#-hidden-code
+_setup()
+//#-end-hidden-code
+//#-editable-code Tap to enter code.
+let line = LinePlot(yData: 0, 1, 4, 9, 16)
+line.label = "/*#-localizable-zone(squaresOfNumbersText)*/Squares of Numbers/*#-end-localizable-zone*/"
+
+//#-end-editable-code
